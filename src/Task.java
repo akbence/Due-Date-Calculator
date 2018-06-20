4c28c7269a467a1774a9bb736cd109354068a9ad
Bence <akbence007@gmail.com>
2018-06-21 00:53:55 +0200

    last refactors

--- src/Task.java
+++ src/Task.java
@@ -6,16 +6,17 @@ public class Task {
 
     /**
      * Calculates the time which is needed by the project
+     *
      * @param calendar Actual date which implements the Calendar interface (e.g. GregorianCalendar).
-     * @param hours The hours which is needed to complete a project.
+     * @param hours    The hours which is needed to complete a project.
      * @return The date in Calendar format.
      */
-    public Calendar whenFinish (Calendar calendar, int hours) {
+    public Calendar whenFinish(Calendar calendar, int hours) {
         checkIfValidDate(calendar);
         Calendar cleanCalendar = clearSecondsAndMilliseconds(calendar);
         int neededDays = calculateDays(hours);
         int neededHours = calculateHours(hours);
-        Calendar finishDateCalendar=updateDaysAndHours(cleanCalendar, neededDays, neededHours);
+        Calendar finishDateCalendar = updateDaysAndHours(cleanCalendar, neededDays, neededHours);
         return finishDateCalendar;
     }
 
@@ -25,14 +26,14 @@ public class Task {
     }
 
     private void checkIfWeekday(Calendar calendar) {
-        if((calendar.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY )
-            || (calendar.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY )){
+        if ((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+                || (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)) {
             throw new IllegalArgumentException("Basedate not in weekday");
         }
     }
 
     private void checkIfWorkingHours(Calendar calendar) {
-        if(calendar.get(Calendar.HOUR)<9 || calendar.get(Calendar.HOUR)>17  ){
+        if (calendar.get(Calendar.HOUR) < 9 || calendar.get(Calendar.HOUR) > 17) {
             throw new IllegalArgumentException("Basedate not in working hours");
         }
     }
@@ -43,48 +44,49 @@ public class Task {
         return updatedHours;
     }
 
-    private Calendar updateDays(int neededDays,Calendar cleanCalendar) {
-        Calendar updatedDays;
-        updatedDays=(Calendar) cleanCalendar.clone();
+    private Calendar updateDays(int neededDays, Calendar cleanCalendar) {
+        Calendar updatedDays = (Calendar) cleanCalendar.clone();
         for (int i = 0; i < neededDays; i++) {
             updatedDays.add(Calendar.DAY_OF_YEAR, 1);
-            updatedDays=addTwoDaysIfSaturday(updatedDays);
+            updatedDays = addTwoDaysIfSaturday(updatedDays);
         }
         return updatedDays;
     }
 
     private Calendar updateHours(int neededHours, Calendar updatedDays) {
-        Calendar updatedHours;
-        updatedHours=(Calendar) updatedDays.clone();
+        Calendar updatedHours = (Calendar) updatedDays.clone();
         for (int i = 0; i < neededHours; i++) {
             updatedHours.add(Calendar.HOUR, 1);
-            updatedHours=addDayIfWorkshiftOver(updatedHours);
+            updatedHours = addDayIfWorkshiftOver(updatedHours);
         }
         return updatedHours;
     }
 
-    private Calendar addDayIfWorkshiftOver(Calendar updatedDays) {
-        if(updatedDays.get(Calendar.HOUR_OF_DAY)==17) {
-            updatedDays.add(Calendar.DAY_OF_YEAR, 1);
-            updatedDays.set(Calendar.HOUR_OF_DAY, 9);
-            updatedDays=addTwoDaysIfSaturday(updatedDays);
+    private Calendar addDayIfWorkshiftOver(Calendar calendar) {
+        Calendar checkedWorkshift = (Calendar) calendar.clone();
+        if (checkedWorkshift.get(Calendar.HOUR_OF_DAY) == 17) {
+            checkedWorkshift.add(Calendar.DAY_OF_YEAR, 1);
+            checkedWorkshift.set(Calendar.HOUR_OF_DAY, 9);
+            checkedWorkshift = addTwoDaysIfSaturday(checkedWorkshift);
         }
-        return  updatedDays;
+        return checkedWorkshift;
     }
 
-    private Calendar addTwoDaysIfSaturday(Calendar updatedDays) {
-        if (updatedDays.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
-            updatedDays.add(Calendar.DAY_OF_YEAR, 2);
+    private Calendar addTwoDaysIfSaturday(Calendar calendar) {
+        Calendar checkIfWeekend = (Calendar) calendar.clone();
+        if (checkIfWeekend.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+            checkIfWeekend.add(Calendar.DAY_OF_YEAR, 2);
         }
-        return updatedDays;
+        return checkIfWeekend;
     }
 
     private int calculateHours(int neededHours) {
-        neededHours=neededHours%8;
+        neededHours = neededHours % 8;
         return neededHours;
     }
+
     private int calculateDays(int neededHours) {
-        int neededDays=neededHours/8;
+        int neededDays = neededHours / 8;
         return neededDays;
     }
 
